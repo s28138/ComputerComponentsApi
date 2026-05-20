b3f22fa8d68f84cc230b0a9cd9986f1c25660045
Bartosz Sitek <sitekbartosz02@gmail.com>
2026-05-20 21:35:40 +0200

    Dodanie konfiguracji połączenia z bazą danych

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using ComputerComponentsApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
