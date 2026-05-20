c0466e56ba1f3855296cbfd7227adefc391f6656
Bartosz Sitek <sitekbartosz02@gmail.com>
2026-05-20 21:31:39 +0200

    Dodanie danych startowych seed data

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -113,5 +113,162 @@ public class AppDbContext : DbContext
                         .HasForeignKey(pcComponent => pcComponent.ComponentCode)
                         .OnDelete(DeleteBehavior.Restrict);
         });
+                SeedData(modelBuilder);
+    }
+    private static void SeedData(ModelBuilder modelBuilder)
+    {
+            modelBuilder.Entity<ComponentManufacturer>().HasData(
+                new ComponentManufacturer
+            {
+                Id = 1,
+                Abbreviation = "AMD",
+                FullName = "Advanced Micro Devices",
+                FoundationDate = new DateOnly(1969, 5, 1)
+            },
+            new ComponentManufacturer
+            {
+                Id = 2,
+                Abbreviation = "NV",
+                FullName = "NVIDIA Corporation",
+                FoundationDate = new DateOnly(1993, 4, 5)
+            },
+            new ComponentManufacturer
+            {
+                Id = 3,
+                Abbreviation = "COR",
+                FullName = "Corsair Gaming Inc.",
+                FoundationDate = new DateOnly(1994, 1, 1)
+            }
+        );
+
+        modelBuilder.Entity<ComponentType>().HasData(
+            new ComponentType
+            {
+                Id = 1,
+                Abbreviation = "CPU",
+                Name = "Processor"
+            },
+            new ComponentType
+            {
+                Id = 2,
+                Abbreviation = "GPU",
+                Name = "Graphics Card"
+            },
+            new ComponentType
+            {
+                Id = 3,
+                Abbreviation = "RAM",
+                Name = "Memory"
+            }
+        );
+
+        modelBuilder.Entity<Component>().HasData(
+            new Component
+            {
+                Code = "CPU0000001",
+                Name = "Ryzen 7 7800X3D",
+                Description = "8-core gaming processor",
+                ComponentManufacturerId = 1,
+                ComponentTypeId = 1
+            },
+            new Component
+            {
+                Code = "GPU0000001",
+                Name = "RTX 4080 Super",
+                Description = "High-end gaming graphics card",
+                ComponentManufacturerId = 2,
+                ComponentTypeId = 2
+            },
+            new Component
+            {
+                Code = "RAM0000001",
+                Name = "Corsair Vengeance DDR5 16GB",
+                Description = "DDR5 RAM module 16GB",
+                ComponentManufacturerId = 3,
+                ComponentTypeId = 3
+            }
+        );
+
+        modelBuilder.Entity<PC>().HasData(
+            new PC
+            {
+                Id = 1,
+                Name = "Gaming Beast X",
+                Weight = 12.5,
+                Warranty = 36,
+                CreatedAt = new DateTime(2026, 5, 8, 9, 0, 0),
+                Stock = 5
+            },
+            new PC
+            {
+                Id = 2,
+                Name = "Office Mini Pro",
+                Weight = 4.2,
+                Warranty = 24,
+                CreatedAt = new DateTime(2026, 4, 15, 13, 30, 0),
+                Stock = 12
+            },
+            new PC
+            {
+                Id = 3,
+                Name = "Creator Station",
+                Weight = 9.8,
+                Warranty = 36,
+                CreatedAt = new DateTime(2026, 3, 20, 10, 15, 0),
+                Stock = 3
+            }
+        );
+
+        modelBuilder.Entity<PCComponent>().HasData(
+            new PCComponent
+            {
+                PCId = 1,
+                ComponentCode = "CPU0000001",
+                Amount = 1
+            },
+            new PCComponent
+            {
+                PCId = 1,
+                ComponentCode = "GPU0000001",
+                Amount = 1
+            },
+            new PCComponent
+            {
+                PCId = 1,
+                ComponentCode = "RAM0000001",
+                Amount = 2
+            },
+            new PCComponent
+            {
+                PCId = 2,
+                ComponentCode = "CPU0000001",
+                Amount = 1
+            },
+            new PCComponent
+            {
+                PCId = 2,
+                ComponentCode = "RAM0000001",
+                Amount = 1
+            },
+            new PCComponent
+            {
+                PCId = 3,
+                ComponentCode = "CPU0000001",
+                Amount = 1
+            },
+            new PCComponent
+            {
+                PCId = 3,
+                ComponentCode = "GPU0000001",
+                Amount = 1
+            },
+            new PCComponent
+            {
+                PCId = 3,
+                ComponentCode = "RAM0000001",
+                Amount = 4
+            }
+        );
     }
 }
+
