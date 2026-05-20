9d4e4b949a8ea55857ad17e54decd9aaf694731c
Bartosz Sitek <sitekbartosz02@gmail.com>
2026-05-20 21:45:22 +0200

    Dodanie klas DTO dla komputerów i komponentów

---
+++ DTOs/ComponentDtos.cs
@@ -0,0 +1,31 @@
+﻿namespace ComputerComponentsApi.DTOs;
+
+public class PcComponentResponseDto
+{
+    public int Amount { get; set; }
+    public ComponentResponseDto Component { get; set; } = null!;
+}
+
+public class ComponentResponseDto
+{
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public ComponentManufacturerResponseDto Manufacturer { get; set; } = null!;
+    public ComponentTypeResponseDto Type { get; set; } = null!;
+}
+
+public class ComponentManufacturerResponseDto
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = null!;
+    public string FullName { get; set; } = null!;
+    public DateOnly FoundationDate { get; set; }
+}
+
+public class ComponentTypeResponseDto
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = null!;
+    public string Name { get; set; } = null!;
+}
---
+++ DTOs/PcDtos.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputerComponentsApi.DTOs;
+
+public class PcResponseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+}
+
+public class PcCreateRequestDto
+{
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = null!;
+
+    [Range(0.01, double.MaxValue)]
+    public double Weight { get; set; }
+
+    [Range(1, int.MaxValue)]
+    public int Warranty { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    [Range(0, int.MaxValue)]
+    public int Stock { get; set; }
+}
+
+public class PcUpdateRequestDto : PcCreateRequestDto
+{
+}
+
+public class PcWithComponentsResponseDto : PcResponseDto
+{
+    public List<PcComponentResponseDto> Components { get; set; } = new();
+}
