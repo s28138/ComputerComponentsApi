e20d8c870447dfa644d5ccd25d80c4737d6785fd
Bartosz Sitek <sitekbartosz02@gmail.com>
2026-05-20 19:11:49 +0200

    Dodanie modeli encji dla komputerów i komponentów

---
+++ Models/Component.cs
@@ -0,0 +1,16 @@
+﻿namespace ComputerComponentsApi.Models;
+
+public class Component
+{
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+
+    public int ComponentManufacturerId { get; set; }
+    public int ComponentTypeId { get; set; }
+
+    public ComponentManufacturer ComponentManufacturer { get; set; } = null!;
+    public ComponentType ComponentType { get; set; } = null!;
+
+    public ICollection<PCComponent> PCComponents { get; set; } = new List<PCComponent>();
+}
